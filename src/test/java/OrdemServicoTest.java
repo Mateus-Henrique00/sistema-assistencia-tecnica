4d30e1a88ce19e7f4357c72ab9de16417184b169
Mateus henrique <Matheushenrique8790@outlook.com>
2026-07-06 20:35:50 -0300

    test: testes unitarios com JUnit 5 para Cliente e OrdemServico

--- src/test/java/OrdemServicoTest.java
+++ src/test/java/OrdemServicoTest.java
@@ -1,14 +1,26 @@
 import model.OrdemServico;
 import model.Status;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class OrdemServicoTest {
 
     @Test
     void criarOS(){
-        OrdemServico ordemServico = new OrdemServico();
-        ordemServico.setDataFinalizada(LocalDate.now());
+        OrdemServico ordemServico = new OrdemServico("IPHONE 14", null, Status.ORCAMENTO, 3000 );
+        assertNotNull(ordemServico.getDataAbertura());
+    }
+
+
+    @Test
+    void verificarStatus(){
+        OrdemServico ordemServico = new OrdemServico("IPHONE 14", null, Status.ORCAMENTO, 3000);
+        ordemServico.setStatus(Status.FINALIZADO);
+        assertEquals(Status.FINALIZADO, ordemServico.getStatus());
     }
 }
